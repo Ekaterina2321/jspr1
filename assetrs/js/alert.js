d8ccd05ca4f610a162dc168e09c439d4602f082b
Ekaterina2321 <ekaterina_sandakova2@mail.ru>
2024-09-03 15:52:22 +0300

    Практика 1

---
+++ assetrs/js/alert.js
@@ -0,0 +1,16 @@
+'use strict';
+
+alert('Я JavaScript');
+
+let firstname =prompt('Введите фамилию', 'firstname');
+alert(`Ваша фамилия ${firstname}`);
+let name =prompt('Введите имя', 'name');
+alert(`Ваша имя ${name}`);
+
+let surname =prompt('Введите отчество', 'surname');
+alert(`Ваша фамилия ${surname}`);
+
+
+let isUser = confirm("Это правда ваше ФИО?");
+alert(isUser);
+
